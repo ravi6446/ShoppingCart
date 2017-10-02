34a0cbddb1d8f30d266ac6ff2802a61ade91c00b
Ravi Bhosle <ravi@Ravis-MacBook-Pro.local>
2017-10-02 15:32:31 +0530

    Implemented shared element while trasitioning beteween Product and ProductDetails

--- app/src/main/java/com/ravi/practiceapp/activities/ProductDetailsActivity.java
+++ app/src/main/java/com/ravi/practiceapp/activities/ProductDetailsActivity.java
@@ -1,11 +1,23 @@
 package com.ravi.practiceapp.activities;
 
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.ravi.practiceapp.R;
 import com.ravi.practiceapp.adapters.ProductDetailsAdapter;
+import com.ravi.practiceapp.utils.PermissionManager;
 
 import java.util.ArrayList;
 
@@ -18,14 +30,55 @@ public class ProductDetailsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
 
+        ImageView mIageVIewShared = findViewById(R.id.imageViewShared);
+        TextView textViewName = findViewById(R.id.textViewName);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        supportPostponeEnterTransition();
+
         ViewPager mViewPager = findViewById(R.id.viewpagerImages);
         CircleIndicator mCircleIndicator = findViewById(R.id.circleindicator);
 
         ArrayList<String> mProductImageList = (ArrayList<String>) getIntent().getSerializableExtra("productImages");
-        getSupportActionBar().setTitle(getIntent().getStringExtra("productName"));
+        textViewName.setText(getIntent().getStringExtra("productName"));
 
-        mViewPager.setAdapter(new ProductDetailsAdapter(mProductImageList,ProductDetailsActivity.this));
+
+        mViewPager.setAdapter(new ProductDetailsAdapter(mProductImageList, ProductDetailsActivity.this));
         mCircleIndicator.setViewPager(mViewPager);
 
+
+        Glide.with(this)
+                .load(getIntent().getStringExtra("productImage"))
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        supportStartPostponedEnterTransition();
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        supportStartPostponedEnterTransition();
+                        return false;
+
+                    }
+                })
+                .into(mIageVIewShared);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
